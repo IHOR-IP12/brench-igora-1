0ded9a9f60c1d8f6cac62dd5dc4647e6053c8978
IHOR-IP12 <i.znetynyak.o@gmail.com>
2023-01-27 18:06:35 +0100

    init

---
+++ sketch_jan11a/sketch_jan11a.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+const int TriggerPin = 8;
+const int EchoPin    = 9;
+long  Duration       = 0;
+int   Delay_timer    = 1000; 
+
+
+void setup() {
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  Serial.begin(9600);
+}
+
+long Distance(long time){
+  long DistanceCalc;
+  DistanceCalc = ((time * 0.034) / 2); // centimeters
+  return DistanceCalc;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TriggerPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+  Duration = pulseIn(EchoPin,HIGH);
+  long Distance_cm = Distance(Duration); 
+  Serial.print("Distance = "); 
+  Serial.print(Distance_cm); 
+  Serial.println (" cm");
+  delay(Delay_timer); 
+}
